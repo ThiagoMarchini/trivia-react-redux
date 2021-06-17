131b8a2a5ea442f2ee7a2f21896ee5f3e30773b3
Thiago Marchini <thiago@msn.com>
2021-06-16 22:38:24 -0300

    Implementado o requisito 8

--- src/reducers/timeout.js
+++ src/reducers/timeout.js
@@ -1,18 +1,31 @@
 const INITIAL_STATE = {
   time: 30,
   timeout: false,
+  show: true,
 };
 
 export default (state = INITIAL_STATE, { type }) => {
   switch (type) {
   case 'TIMEOUT':
     return {
+      ...state,
       timeout: true,
     };
   case 'RESET':
     return {
       time: 30,
       timeout: false,
+      show: true,
+    };
+  case 'DECREASE':
+    return {
+      ...state,
+      time: state.time - 1,
+    };
+  case 'HIDE':
+    return {
+      ...state,
+      show: false,
     };
   default:
     return state;
